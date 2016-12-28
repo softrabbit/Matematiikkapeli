0a6700eb5fee7f0f30d198bfb616d140fb3c46ea
Raine M. Ekman <raine@iki.fi>
2016-12-28 23:31:55 +0200

    Basic layout of game, logic missing

---
+++ Assets/UpdateText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateText : MonoBehaviour {
+	UnityEngine.UI.Text score;
+
+	// Use this for initialization
+	void Start () {
+		score = GetComponent<UnityEngine.UI.Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		score.text = "FOO!";
+	}
+}
